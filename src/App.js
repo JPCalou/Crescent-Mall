127b584ef687e1ccf2d80b8b3706d182c683771c
Juan Pedro Calou <jpcalou23@gmail.com>
2022-08-16 20:23:13 -0300

    creacion de NavBar con Logo

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import NavBar from './Components/NavBar.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Crescent Mall</h1>
+      
+        
         <NavBar></NavBar>
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
       
       
-      </header>
+    
     </div>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,27 +3,26 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from './Imagenes/logo-mkr.png';
+
+
 
 function NavBar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="primary" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+      <Navbar.Brand href="#home"><img src={logo} width="60" height="60" alt="logo"/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#home">Crescent Market</Nav.Link>
             <Nav.Link href="#link">Contacto</Nav.Link>
             <NavDropdown title="Productos" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Velas</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Jabones
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Jabones</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Fragancias</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Mas info ...
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Mas info ...</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
